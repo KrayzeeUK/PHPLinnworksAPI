4912e1aabcfea7d76df9cf5a4c653152dbb9c9dd
KrayzeeUK <71898826+KrayzeeUK@users.noreply.github.com>
2021-11-16 12:22:53 +0000

    Update api_linnworks.php

--- api_linnworks.php
+++ api_linnworks.php
@@ -989,7 +989,7 @@
 
 		function CustomSearchByCategoryAndKeyword( $keyword, string $category = "'Default'", $ignore = "", $bcSKUcheck = FALSE ) {
 
-			$category = str_ireplace( '"', "'", $category );
+			$category = str_ireplace( array( '"', "\r", "\n" ), array( "'", "", "" ), $category );
 
 			if ( strpos( $category, "'" ) != 0 ) {
 				$category = "'" . $category;
